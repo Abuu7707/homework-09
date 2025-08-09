caf4095c0ee2469f6e3bb02f3d30fcdb8f03bb63
Abuu7707 <rahmatovv7707@gmai.com>
2025-08-09 09:25:33 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int Sum(int number)
+{
+    if (number > 1)
+    {
+        Sum(number - 1);
+    }
+    System.Console.Write(number + " ");
+    return 0;
+}
+int a = Convert.ToInt32(System.Console.ReadLine());
+Sum(a);
